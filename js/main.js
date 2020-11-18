6c7da55df199d4f7365a6f286ceecdae26efba72
petern1953 <petern1953@gmail.com>
2020-11-18 20:32:10 +0100

    vissza a 2 órával ezelőtti állapothoz

--- js/main.js
+++ js/main.js
@@ -1,15 +1,14 @@
-let hider = document.querySelector('#hider');
-// let modalContainer = document.querySelector('#modal__container');
+let modalContainer = document.querySelector('#modal__container');
 let initBtn = document.querySelector('#init-button');
 
-initBtn.addEventListener('click', showModal);
+(function initShowModalBtn() {
+    initBtn.addEventListener('click', showModal);
+})()
 
 function showModal() {
-    hider.setAttribute('class', 'hider--visible')
-    // modalContainer.setAttribute('class', 'modal__container--visible')
+    modalContainer.setAttribute('class', 'modal__container--visible')
 }
 
 function hideModal() {
-    hider.setAttribute('class', 'hider--hidden')
-    // modalContainer.setAttribute('class', 'modal__container--hidden')
+    modalContainer.setAttribute('class', 'modal__container--hidden')
 }

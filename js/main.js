11e0ef5eb56b574c69d0b7aa23a78193a30d48bc
petern1953 <petern1953@gmail.com>
2020-11-18 19:43:47 +0100

    showModal hideModal modalContainer out

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 let hider = document.querySelector('#hider');
+// let modalContainer = document.querySelector('#modal__container');
 let initBtn = document.querySelector('#init-button');
 
 initBtn.addEventListener('click', showModal);
 
 function showModal() {
-    modalContainer.setAttribute('class', 'hider--visible')
+    hider.setAttribute('class', 'hider--visible')
+    // modalContainer.setAttribute('class', 'modal__container--visible')
 }
 
 function hideModal() {
-    modalContainer.setAttribute('class', 'hider--hidden')
+    hider.setAttribute('class', 'hider--hidden')
+    // modalContainer.setAttribute('class', 'modal__container--hidden')
 }

2cf341e8b08de719f0d8f8d189d48fea87fc9531
petern1953 <petern1953@gmail.com>
2020-11-19 08:11:32 +0100

    --outPosition: -100vw

--- js/main1.js
+++ js/main1.js
@@ -1,9 +1,11 @@
-
 let modalContainer = document.querySelector('#modal__container');
-let hider = document.querySelector('#hider');
+// let hider = document.querySelector('#hider');
 let initBtn = document.querySelector('#init-button');
 
 initBtn.addEventListener('click', showModal);
+
+const hiders = document.querySelectorAll('hider');
+hiders.forEach(hider => hider.addEventListener('click', hideModal));
 
 function showModal() {
     modalContainer.setAttribute('class', 'modal__container--visible')

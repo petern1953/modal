df7126c5bed74105e3d73cdaa98ed22148673f95
petern1953 <petern1953@gmail.com>
2020-11-20 17:25:23 +0100

    focus() set to OKbutton on modal

--- js/main.js
+++ js/main.js
@@ -3,13 +3,14 @@ const modalContainer = document.querySelector('#modal__container');
 const hider = document.querySelector('#hider');
 const initBtn = document.querySelector('#init-button');
 const modal = document.querySelector('#modal');
+const okModalButton = document.querySelector(".button--ok");
 
 initBtn.addEventListener('click', showModal);
 
 function showModal() {
     modalContainer.setAttribute('class', 'modal__container--visible');
     hider.setAttribute('class', 'hider--visible');
-    modal.focus();
+    okModalButton.focus();
 }
 
 function hideModal() {
